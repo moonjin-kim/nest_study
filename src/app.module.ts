73b88ca4a3e14b5f41e173dc97b281a81ca03282
moonjin-kim <ace39418@gmail.com>
2024-04-16 23:10:58 +0900

    build: typeorm setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 전체적으로 사용하기 위해
+      envFilePath: process.env.NODE_ENV === 'dev' ? './env/.env.dev' : './env/.env.test',
+    }),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.APP_DATABASE,
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+      logging: process.env.DB_LOGGING === 'true', 
+      dropSchema: process.env.DB_DROP_SCHEMA === 'true',
+      entities: [
+        "src/entity/**/*.ts",
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
