608387c18b1f3695c64f98a34bcd785d14b4b14c
moonjin-kim <ace39418@gmail.com>
2024-04-19 23:03:24 +0900

    test: controller 학습 테스트"

---
+++ src/controller/user/user.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { RegisterUserDto } from "./request/register-user.dto";
+
+@Controller('user')
+export class UserController {
+    @Post('')
+    register(@Body() registerUserDto: RegisterUserDto) {
+        return '회원가입 되었습니다.'
+    }
+
+    @Get('')
+    find() {
+        return RegisterUserDto.create(
+            'test@test.com',
+            'testpassword123',
+            'test-key',
+            '김문진',
+        );
+    }
+
+}
